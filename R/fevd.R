fb6c1b692c4b9f5bc85d4832e7caa1bcc180b608
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-05-05 23:28:20 +0100

    remove section from documentation

--- R/fevd.R
+++ R/fevd.R
@@ -3,8 +3,6 @@
 #' @param object A fitted var object.
 #' @param ... Other arguments passed to methods
 #'
-#' @section Methods:
-#' \Sexpr[stage=render,results=rd]{generics:::methods_rd("fevd")}
 #'
 #' @export
 fevd <- function(object, ...) {
--- R/irf.R
+++ R/irf.R
@@ -1,10 +1,7 @@
 #' Impulse Response Functions
 #'
 #' @param object A fitted var object.
-#' @param ... Other arguments passed to methods
-#'
-#' @section Methods:
-#' \Sexpr[stage=render,results=rd]{generics:::methods_rd("irf")}
+#' @param ... Other arguments passed to method
 #'
 #' @export
 irf <- function(object, ...) {
